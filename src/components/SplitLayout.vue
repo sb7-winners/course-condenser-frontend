676ba4015be181418904f4fc9ffada84119ecedc
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-01-16 22:47:10 -0800

    Split layout component

---
+++ src/components/SplitLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="splitLayoutContainer">
+    <div class="pane-container">
+      <slot name="primary"></slot>
+    </div>
+    <div class="pane-container">
+      <slot name="secondary"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#splitLayoutContainer {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+
+.pane-container {
+  width: 50%;
+}
+</style>
